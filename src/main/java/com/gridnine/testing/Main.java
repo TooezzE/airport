e0a37dd72c321f1ed2c7be528dd670fbd2172ac2
TooezzE <azazaz.com534@gmail.com>
2024-01-11 18:13:57 +0300

    init

---
+++ src/main/java/com/gridnine/testing/Main.java
@@ -0,0 +1,16 @@
+package com.gridnine.testing;
+
+
+import com.gridnine.testing.builder.FlightBuilder;
+import com.gridnine.testing.filters.impl.ArrivalBeforeDepartureFilter;
+import com.gridnine.testing.filters.impl.LongTimeWaitingFilter;
+import com.gridnine.testing.filters.impl.PastFlightsFilter;
+import com.gridnine.testing.model.Flight;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
