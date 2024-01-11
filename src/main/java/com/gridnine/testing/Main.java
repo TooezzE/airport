514f084b4945cf182e0103837727ff08cf05eae9
TooezzE <azazaz.com534@gmail.com>
2024-01-11 18:15:39 +0300

    done.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -11,6 +11,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String delimiter = "----------------";
+        List<Flight> allFlights = FlightBuilder.createFlights();
+
+        System.out.println("All flights:");
+        System.out.println(allFlights);
+        System.out.println(delimiter);
+
+        System.out.println("Already finished flights:");
+        System.out.println(new PastFlightsFilter().filter(allFlights));
+        System.out.println(delimiter);
+
+        System.out.println("Flights which arrival before departure:");
+        System.out.println(new ArrivalBeforeDepartureFilter().filter(allFlights));
+        System.out.println(delimiter);
+
+        System.out.println("Long time waiting flights:");
+        System.out.println(new LongTimeWaitingFilter().filter(allFlights));
     }
 }
